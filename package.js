75388a3aa9b1fb6e05b095f6bbf1ff7a34367fa1
Serut <l.mieulet@gmail.com>
2016-04-14 11:48:39 +0200

    Increment atmosphere version

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'lmieulet:meteor-coverage',
-  version: '0.2.0',
+  version: '0.2.1',
   // Brief, one-line summary of the package.
   summary: 'Server and client coverage for Meteor',
   // URL to the Git repository containing the source code for this package.
