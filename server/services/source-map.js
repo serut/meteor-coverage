09831181d54678eaa86d02267dde9ccdf961d840
Carlos García <cgalvarez.engineer@gmail.com>
2016-09-09 16:46:52 +0200

    fix not considered source map path patterns

--- server/services/source-map.js
+++ server/services/source-map.js
@@ -8,9 +8,13 @@ const libSourceMaps = istanbulAPI.libSourceMaps;
 const sourceMap = libSourceMaps.createSourceMapStore({verbose: Conf.IS_COVERAGE_ACTIVE});
 const meteor_dir = Conf.COVERAGE_APP_FOLDER;
 // Next RegExps must consider following paths:
-//  - meteor://💻app/packages/pkgAuthor[_:]pkgName/...
-//  - meteor://💻app/packages/local-test:pkgAuthor:pkgName/...
-const regexAlterationSourceMapPath = new RegExp(/(packages\/)(?:local-test[_:])?([a-zA-Z-]*)[_:]([a-zA-Z-_]*)(.*)/);
+//  1. meteor://💻app/packages/lmieulet:meteor-coverage/server/index.js
+//  2. meteor://💻app/.npm/package/node_modules/minimatch/package.json
+//  3. meteor://💻app/node_modules/meteor/lmieulet:meteor-coverage/node_modules/minimatch/minimatch.js
+//  4. meteor://💻app/packages/local-test:lmieulet:meteor-coverage/server/tests.js
+//  5. meteor://💻app/node_modules/meteor/local-test:cgalvarez:my-package/tests/client/mocks.js
+//  6. meteor://💻app/node_modules/meteor/local-test:cgalvarez:my-package/node_modules/chai-as-promised/lib/chai-as-promised.js
+const regexAlterationSourceMapPath = new RegExp(/^(packages\/|node_modules\/meteor\/)(?:local-test[_:])?([a-zA-Z-]*)[_:]([a-zA-Z-_]*)(.*)$/);
 const regexPUT = new RegExp(/packages\/local-test:([a-zA-Z-_]*):([a-zA-Z-_]*)\/.*/);
 
 // Alter inside the source map the path of each sources
@@ -35,6 +39,8 @@ alterSourceMapPaths = function (map) {
     }
   }
 
+  const npmPkgFolder = "node_modules";
+  const npmPkgDepPrefix = `${npmPkgFolder}/meteor/`;
   const splitToken = String.fromCharCode(56507) + 'app/';
   for (var i = 0; i < map.sources.length; i++) {
     // Magic character inside the path
@@ -46,10 +52,21 @@ alterSourceMapPaths = function (map) {
 
       if (match) {
         if (Meteor.PUT && match[2] === Meteor.PUT.author && match[3] === Meteor.PUT.name) { // PUT file
+          // Imported NPM dependency (skip initial backslash)
+          if (match[1] === npmPkgDepPrefix && match[4].startsWith(npmPkgFolder, 1)) {
+            map.sources[i] = `${meteor_dir}.npm/package${match[4]}`;
+          // Custom user file imported in test file or package loaded for app or meteor framework
+          } else {
+            map.sources[i] = meteor_dir + match[4].substr(1);
+          }
+        } else {
+          map.sources[i] = meteor_dir + match[1] + match[3] + match[4];
+        }
+        /*if (Meteor.PUT && match[2] === Meteor.PUT.author && match[3] === Meteor.PUT.name) { // PUT file
           map.sources[i] = meteor_dir + match[4].substr(1);
         } else { // package loaded for app or meteor framework
           map.sources[i] = meteor_dir + match[1] + match[3] + match[4];
-        }
+        }*/
       } else {
         map.sources[i] = meteor_dir + path;
       }
