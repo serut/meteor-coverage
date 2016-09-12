647b6e6e87edbd82487ba3ca378db0b775ae1781
Serut <l.mieulet@gmail.com>
2016-09-13 00:05:26 +0200

    Deploy 0.9.6 with - new configuration file - internal behavior fixed - new readme

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'lmieulet:meteor-coverage',
-  version: '0.9.4',
+  version: '0.9.6',
   summary: 'Server and client coverage for Meteor',
   git: 'https://github.com/serut/meteor-coverage',
   documentation: 'README.md',
