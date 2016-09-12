4cd9a9fccd588f28e707b574b46bc37b0f38dcb9
Carlos García <cgalvarez.engineer@gmail.com>
2016-09-12 13:31:15 +0200

    fix coverage verbosity

--- server/context/log.js
+++ server/context/log.js
@@ -1,23 +1,22 @@
-export const COVERAGE_VERBOSE = process.env['COVERAGE_VERBOSE'] === '1' || false;
-
 export default Log = {
+  COVERAGE_VERBOSE: process.env['COVERAGE_VERBOSE'] === '1' || false,
   error: function() {
-    if (COVERAGE_VERBOSE) {
+    if (this.COVERAGE_VERBOSE) {
       console.error(...arguments);
     }
   },
   info: function() {
-    if (COVERAGE_VERBOSE) {
+    if (this.COVERAGE_VERBOSE) {
       console.log(...arguments);
     }
   },
   time: function() {
-    if (COVERAGE_VERBOSE) {
+    if (this.COVERAGE_VERBOSE) {
       console.log(...arguments);
     }
   },
   timeEnd: function() {
-    if (COVERAGE_VERBOSE) {
+    if (this.COVERAGE_VERBOSE) {
       console.log(...arguments);
     }
   }
